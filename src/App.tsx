47e6b1c153818bfcd498165f21874a0d1f4f4526
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-08-17 10:16:18 -0400

    Início da programação do footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Header from './componentes/header/Header'
-
+import Footer from './componentes/footer/Footer'
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   )
 }
 export default App
