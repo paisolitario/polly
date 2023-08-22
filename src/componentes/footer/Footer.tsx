9cb13f274fe0a9cc4f8e98da04db31eb89f1542c
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-08-22 08:06:37 -0400

    Iniciando a programação do footer

--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -1,7 +1,9 @@
 import './Footer.css'
+import Logo from '../../assets/pngfind.com-barbie-logo-png-667657.png'
 function Footer(){
     return(
         <footer>
+            <img src={Logo} alt="" />
         </footer>
     )
 }
