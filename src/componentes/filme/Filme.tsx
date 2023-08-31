400abae3b73efeecf904f99ad4243f99545266dd
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-08-31 09:48:08 -0400

    Adicionando a imagem da barbie

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,7 +1,11 @@
 export default function Filme(){
     return(
         <div className="filme_content">
-            Filme
+            <img src="/barbie.png" alt=""/>
+            <div className="texto_foto">
+                <h1>Título</h1>
+                <p className="sinopse">Blablablabla</p>
+            </div>
         </div>
     )
 }
