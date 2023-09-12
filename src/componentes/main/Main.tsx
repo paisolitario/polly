91753eb5fd119b0f836aafb1554de260da1275fc
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-09-12 08:26:42 -0400

    Inclusão de um vetor de filmes no main.tsx

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import Filme from "../filme/Filme";
 import './Main.css'
+type FilmesType = {
+    id: number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
 export default function Main(){
-    let texto = "Guilherme"
-    function mudaTexto(){
-        console.log("tere")
-        texto = "Tere"
+    //Hook
+    const [texto,setTexto] = useState("Guilherme")
+
+    const filmes:FilmesType[] = [
+        {
+            id:1,
+            titulo:"Barbie",
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:"/barbie.png"
+        },
+        {
+            id:2,
+            titulo:"Transformers",
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:"/transformes.jpeg"
+        }
+
+    ]
+    //A função recebe um atributo chamado e de "event"
+    function mudaTexto(e:React.ChangeEvent<HTMLInputElement>){
+        console.log(e.target.value)
+        setTexto(e.target.value)
     }
     return(
         <>
