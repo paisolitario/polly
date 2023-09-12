092d45686b7452c97625fb641c0b7dda66f314a9
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-09-12 07:44:25 -0400

    Mudança da barra de pesquisa para o main.tsx

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -7,60 +7,66 @@ export default function Main(){
         texto = "terenciani"
     }
     return(
-        <main className="content-main">
-            <input className='barrapesquisa' type="text" onChange={mudaTexto} />
-            <div>
-                <p className='texto_digitado'>pesquisa: {texto}</p>
+        <>
+            <div className="pesquisa">
+                
+                <p>Buscar Filme</p>
+                <input className='barrapesquisa' type="text" onChange={mudaTexto} />
+                <div>
+                    <p className='texto_digitado'>pesquisa: {texto}</p>
+                </div>
             </div>
-            <Filme titulo="Barbie" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/barbie.png"
-                    />
-            <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/transformes.jpeg"
-                    />
-            <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/transformes.jpeg"
-                    />
-            <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/transformes.jpeg"
-                    />
-            <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/transformes.jpeg"
-                    />
-            <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
-                    Barbieland por ser uma boneca 
-                    de aparência menos do que 
-                    perfeita, Barbie parte para o 
-                    mundo humano em busca da 
-                    verdadeira felicidade."
-                    imagem="/transformes.jpeg"
-                    />
+            <main className="content-main">
+                <Filme titulo="Barbie" sinopse="Depois de ser expulsa da 
+                        Barbieland por ser uma boneca 
+                        de aparência menos do que 
+                        perfeita, Barbie parte para o 
+                        mundo humano em busca da 
+                        verdadeira felicidade."
+                        imagem="/barbie.png"
+                        />
+                <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
+                        Barbieland por ser uma boneca 
+                        de aparência menos do que 
+                        perfeita, Barbie parte para o 
+                        mundo humano em busca da 
+                        verdadeira felicidade."
+                        imagem="/transformes.jpeg"
+                        />
+                <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
+                        Barbieland por ser uma boneca 
+                        de aparência menos do que 
+                        perfeita, Barbie parte para o 
+                        mundo humano em busca da 
+                        verdadeira felicidade."
+                        imagem="/transformes.jpeg"
+                        />
+                <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
+                        Barbieland por ser uma boneca 
+                        de aparência menos do que 
+                        perfeita, Barbie parte para o 
+                        mundo humano em busca da 
+                        verdadeira felicidade."
+                        imagem="/transformes.jpeg"
+                        />
+                <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
+                        Barbieland por ser uma boneca 
+                        de aparência menos do que 
+                        perfeita, Barbie parte para o 
+                        mundo humano em busca da 
+                        verdadeira felicidade."
+                        imagem="/transformes.jpeg"
+                        />
+                <Filme titulo="Transformers" sinopse="Depois de ser expulsa da 
+                        Barbieland por ser uma boneca 
+                        de aparência menos do que 
+                        perfeita, Barbie parte para o 
+                        mundo humano em busca da 
+                        verdadeira felicidade."
+                        imagem="/transformes.jpeg"
+                        />
 
-        </main>
+            </main>
+        </>
     )
 }
