9ae329e1ed5d298e00811f8f9aec68e6fc19fcc4
guilhermeterenciani <guilhermeterenciani@gmail.com>
2023-09-28 09:40:21 -0400

    Refatorando código da página inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import App from './App.tsx'
+import './main.css'
+import Header from './componentes/header/Header'
+import Footer from './componentes/footer/Footer'
+import Main from './componentes/main/Main'
 import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
@@ -8,7 +11,27 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <>
+              <Header/>
+              <Main/>
+              <Footer/>
+             </>,
+  },
+  {
+    path: "/home",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/sobre",
+    element: <div>Página Sobre!</div>,
+  },
+  {
+    path: "/noticias",
+    element: <div>Página de Notícias!</div>,
+  },
+  {
+    path: "/contato",
+    element: <div>Página de Contato!</div>,
   },
 ]);
 
